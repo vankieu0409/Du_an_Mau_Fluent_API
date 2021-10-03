93b2ec35b787f4b55482a20b37dc887198cc76f7
Kiều Nguyễn <beu_csnv@outlook.com>
2021-10-03 15:43:55 +0700

    123456

--- DAM_Fluent_API/GUI_QLBH/frmKhachHang.Designer.cs
+++ DAM_Fluent_API/GUI_QLBH/frmKhachHang.Designer.cs
@@ -29,6 +29,9 @@ namespace GUI_QLBH
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
@@ -48,86 +51,137 @@ namespace GUI_QLBH
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_lisst)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(8, 7);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1008, 590);
+            this.tabControl1.TabIndex = 44;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.DGV_lisst);
+            this.tabPage1.Controls.Add(this.rbtn_KhongHD);
+            this.tabPage1.Controls.Add(this.rbtn_HoatDong);
+            this.tabPage1.Controls.Add(this.txt_Address);
+            this.tabPage1.Controls.Add(this.txt_name);
+            this.tabPage1.Controls.Add(this.txt_Email);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1000, 557);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(987, 557);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.Location = new System.Drawing.Point(844, 535);
+            this.button8.Location = new System.Drawing.Point(824, 485);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(94, 38);
-            this.button8.TabIndex = 43;
+            this.button8.TabIndex = 62;
             this.button8.Text = "Close";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Location = new System.Drawing.Point(720, 535);
+            this.button7.Location = new System.Drawing.Point(700, 485);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(94, 38);
-            this.button7.TabIndex = 42;
+            this.button7.TabIndex = 61;
             this.button7.Text = "List";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(592, 535);
+            this.button6.Location = new System.Drawing.Point(572, 485);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(94, 38);
-            this.button6.TabIndex = 41;
+            this.button6.TabIndex = 60;
             this.button6.Text = "Skip";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(465, 535);
+            this.button5.Location = new System.Drawing.Point(445, 485);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(94, 38);
-            this.button5.TabIndex = 40;
+            this.button5.TabIndex = 59;
             this.button5.Text = "Save";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(341, 535);
+            this.button4.Location = new System.Drawing.Point(321, 485);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(94, 38);
-            this.button4.TabIndex = 39;
+            this.button4.TabIndex = 58;
             this.button4.Text = "Edit";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(215, 535);
+            this.button3.Location = new System.Drawing.Point(195, 485);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 38);
-            this.button3.TabIndex = 38;
+            this.button3.TabIndex = 57;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(90, 535);
+            this.button2.Location = new System.Drawing.Point(70, 485);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 38);
-            this.button2.TabIndex = 37;
+            this.button2.TabIndex = 56;
             this.button2.Text = "Create";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(592, 463);
+            this.button1.Location = new System.Drawing.Point(572, 413);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 38);
-            this.button1.TabIndex = 36;
+            this.button1.TabIndex = 55;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -135,30 +189,30 @@ namespace GUI_QLBH
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.Window;
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox4.Location = new System.Drawing.Point(354, 463);
+            this.textBox4.Location = new System.Drawing.Point(334, 413);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(218, 38);
-            this.textBox4.TabIndex = 35;
+            this.textBox4.TabIndex = 54;
             // 
             // DGV_lisst
             // 
             this.DGV_lisst.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.DGV_lisst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_lisst.Location = new System.Drawing.Point(58, 182);
+            this.DGV_lisst.Location = new System.Drawing.Point(38, 132);
             this.DGV_lisst.Name = "DGV_lisst";
             this.DGV_lisst.RowHeadersWidth = 51;
             this.DGV_lisst.RowTemplate.Height = 29;
             this.DGV_lisst.Size = new System.Drawing.Size(924, 254);
-            this.DGV_lisst.TabIndex = 34;
+            this.DGV_lisst.TabIndex = 53;
             // 
             // rbtn_KhongHD
             // 
             this.rbtn_KhongHD.AutoSize = true;
-            this.rbtn_KhongHD.Location = new System.Drawing.Point(736, 129);
+            this.rbtn_KhongHD.Location = new System.Drawing.Point(716, 79);
             this.rbtn_KhongHD.Name = "rbtn_KhongHD";
             this.rbtn_KhongHD.Size = new System.Drawing.Size(50, 24);
-            this.rbtn_KhongHD.TabIndex = 33;
+            this.rbtn_KhongHD.TabIndex = 52;
             this.rbtn_KhongHD.TabStop = true;
             this.rbtn_KhongHD.Text = "Nữ";
             this.rbtn_KhongHD.UseVisualStyleBackColor = true;
@@ -167,10 +221,10 @@ namespace GUI_QLBH
             // 
             this.rbtn_HoatDong.AutoSize = true;
             this.rbtn_HoatDong.Checked = true;
-            this.rbtn_HoatDong.Location = new System.Drawing.Point(592, 129);
+            this.rbtn_HoatDong.Location = new System.Drawing.Point(572, 79);
             this.rbtn_HoatDong.Name = "rbtn_HoatDong";
             this.rbtn_HoatDong.Size = new System.Drawing.Size(62, 24);
-            this.rbtn_HoatDong.TabIndex = 32;
+            this.rbtn_HoatDong.TabIndex = 51;
             this.rbtn_HoatDong.TabStop = true;
             this.rbtn_HoatDong.Text = "Nam";
             this.rbtn_HoatDong.UseVisualStyleBackColor = true;
@@ -178,68 +232,68 @@ namespace GUI_QLBH
             // txt_Address
             // 
             this.txt_Address.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_Address.Location = new System.Drawing.Point(592, 84);
+            this.txt_Address.Location = new System.Drawing.Point(572, 34);
             this.txt_Address.Multiline = true;
             this.txt_Address.Name = "txt_Address";
             this.txt_Address.Size = new System.Drawing.Size(298, 27);
-            this.txt_Address.TabIndex = 29;
+            this.txt_Address.TabIndex = 50;
             // 
             // txt_name
             // 
             this.txt_name.BackColor = System.Drawing.SystemColors.Window;
             this.txt_name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_name.Location = new System.Drawing.Point(188, 126);
+            this.txt_name.Location = new System.Drawing.Point(168, 76);
             this.txt_name.Name = "txt_name";
             this.txt_name.Size = new System.Drawing.Size(218, 27);
-            this.txt_name.TabIndex = 28;
+            this.txt_name.TabIndex = 49;
             // 
             // txt_Email
             // 
             this.txt_Email.BackColor = System.Drawing.SystemColors.Window;
             this.txt_Email.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_Email.Location = new System.Drawing.Point(188, 86);
+            this.txt_Email.Location = new System.Drawing.Point(168, 36);
             this.txt_Email.Name = "txt_Email";
             this.txt_Email.Size = new System.Drawing.Size(218, 27);
-            this.txt_Email.TabIndex = 27;
+            this.txt_Email.TabIndex = 48;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(501, 130);
+            this.label5.Location = new System.Drawing.Point(481, 80);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 25);
-            this.label5.TabIndex = 26;
+            this.label5.TabIndex = 47;
             this.label5.Text = "Giới Tính";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(501, 84);
+            this.label4.Location = new System.Drawing.Point(481, 34);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 25);
-            this.label4.TabIndex = 25;
+            this.label4.TabIndex = 46;
             this.label4.Text = "Địa Chỉ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(86, 124);
+            this.label2.Location = new System.Drawing.Point(66, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 25);
-            this.label2.TabIndex = 23;
+            this.label2.TabIndex = 45;
             this.label2.Text = "Họ và tên";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(86, 84);
+            this.label1.Location = new System.Drawing.Point(66, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 25);
-            this.label1.TabIndex = 22;
+            this.label1.TabIndex = 44;
             this.label1.Text = "Điện Thoại";
             // 
             // frmKhachHang
@@ -247,35 +301,21 @@ namespace GUI_QLBH
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1015, 599);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.DGV_lisst);
-            this.Controls.Add(this.rbtn_KhongHD);
-            this.Controls.Add(this.rbtn_HoatDong);
-            this.Controls.Add(this.txt_Address);
-            this.Controls.Add(this.txt_name);
-            this.Controls.Add(this.txt_Email);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "frmKhachHang";
             this.Text = "Khách Hàng";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_lisst)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
@@ -295,6 +335,6 @@ namespace GUI_QLBH
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
